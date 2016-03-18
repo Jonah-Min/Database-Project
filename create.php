6a05de6095576efc017939ab80c4bc898c2f8b2f
Jonah-Min <jonahm2472@gmail.com>
2016-03-18 11:22:55 -0400

    Formatting Create
    New formatting for create page input form.

--- create.php
+++ create.php
@@ -12,32 +12,64 @@
 	</div>	
 
 	<form method='post' action='update.php'>
-		<div id='newchampform'>
-			<h3>Champion Name</h3>
-			<input type='text' name='champion' placeholder='Champion Name'>
+		<table>
+			<tr>
+				<td><h3>Champion Name</h3></td>
+				<td><input type='text' name='champion' placeholder='Champion Name'></td>
+			</tr>
 
-			<h3>Passive</h3>
-			<input type='text' name='passive' placeholder='Passive'>
+			<tr>
+				<td><h3>Primary Role</h3></td>
+				<td><input type='text' name='primary' placeholder='Primary Role'></td>
+			</tr>
 
-			<h3>Champion Skills</h3>
-			<input type='text' name='skill1' placeholder='Skill 1'><br>
+			<tr>
+				<td><h3>Secondary Role</h3></td>
+				<td><input type='text' name='secondary' placeholder='Secondary Role'></td>
+			</tr>	
 
-			<textarea rows='3' cols='35' name='description1' placeholder='Skill 1 Description'></textarea><br>
+			<tr>
+				<td><h3>Passive</h3></td>
+				<td><input type='text' name='passive' placeholder='Passive'></td>
+			</tr>
 
-			<input type='text' name='skill2' placeholder='Skill 2'><br>
+			<tr>
+				<td colspan='2'><h3>Champion Skills</h3></td>
+			</tr>
 
-			<textarea rows='3' cols='35' name='description2' placeholder='Skill 2 Description'></textarea><br>
+			<tr>
+				<td><input type='text' name='skill1' placeholder='Skill 1' class='skill'><br></td>
+				<td>
+					<textarea rows='3' cols='35' name='description1' placeholder='Skill 1 Description'></textarea><br>
+				</td>
+			</tr>	
 
-			<input type='text' name='skill3' placeholder='Skill 3'><br>
-			
-			<textarea rows='3' cols='35' name='description3' placeholder='Skill 3 Description'></textarea><br>
+			<tr>
+				<td><input type='text' name='skill2' placeholder='Skill 2' class='skill'><br></td>
+				<td>
+					<textarea rows='3' cols='35' name='description2' placeholder='Skill 2 Description'></textarea><br>
+				</td>
+			</tr>	
 
-			<input type='text' name='skill4' placeholder='Skill 4'><br>
+			<tr>
+				<td><input type='text' name='skill3' placeholder='Skill 3' class='skill'><br></td>
+				<td>
+					<textarea rows='3' cols='35' name='description3' placeholder='Skill 3 Description'></textarea><br>
+				</td>
+			</tr>	
 
-			<textarea rows='3' cols='35' name='description4' placeholder='Skill 4 Description'></textarea><br>
+			<tr>
+				<td><input type='text' name='skill4' placeholder='Skill 4' class='skill'><br></td>
+				<td>
+					<textarea rows='3' cols='35' name='description4' placeholder='Skill 4 Description'></textarea><br>
+				</td>
+			</tr>	
 
-			<input type='submit' value='Create New Champion!'>
-		</div>
+			<tr>
+				<td colspan='2'><input type='submit' value='Create New Champion!'></td>
+			</tr>
+
+	</table>
 	</form>	
 
 </body>
