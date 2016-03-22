ea42b51cd37dabe465078f98770f3afcc94cf94d
Jonah-Min <jonahm2472@gmail.com>
2016-03-21 21:18:04 -0400

    Results Page
    Added results page and working on styling

--- result.php
+++ result.php
@@ -1,7 +1,90 @@
 <?php
 
+	$connect = mysql_connect('localhost', 'root', '');
+
+	if (!$connect) {
+		die('Could Not Connect: ' . mysql_error());
+	}
+
+	mysql_select_db('league');
+
 	$value = $_POST['button'];
 
-	echo $value;
+	$skinquery = "SELECT skinname, url FROM skins WHERE champname='$value'";
+	$result = mysql_query($skinquery);
+
+	$skins = array();
+
+	while ($row = mysql_fetch_array($result)) {
+		array_push($skins, $row['url']);
+	}
+
+	$champinfo = "SELECT primaryrole, secondaryrole FROM champion WHERE champname='$value'";
+	$result = mysql_query($champinfo);
+	$info = mysql_fetch_array($result);
+
+	$primary = $info['primaryrole'];
+	$secondary = $info['secondaryrole'];
+
+	$skills = "SELECT * FROM skills WHERE champname='$value'";
+	$skills = mysql_query($skills);
+
 
 ?>
+
+<html>
+
+<head>
+
+	<link rel='stylesheet' type='text/css' href='result.css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+
+	<style>
+		body {
+			background: url(<?php echo $skins[0]?>);
+			background-attachment: fixed;
+			background-size: 100%;
+		}
+	</style>
+
+</head>
+
+<body>
+
+	<div id='container'>
+		<p id='name'> <?php echo $value ?> </p><br><br><br><br>
+
+		<span class='role'><?php echo $primary ?> </span><br>
+		<span class='role'><?php echo $secondary ?> </span>
+
+		<table id='skills'>
+			<tr>
+				<td colspan='5'><p class='title'>Skills</p></td>
+			</tr>
+			<tr>
+				<td><p class='skillid'>P</p></td>
+				<td><p class='skillid'>Q</p></td>
+				<td><p class='skillid'>W</p></td>
+				<td><p class='skillid'>E</p></td>
+				<td><p class='skillid'>R</p></td>
+			</tr>
+			<tr>
+				<?php
+
+					while ($row = mysql_fetch_array($skills)) {
+						echo "<td align='center' class='tablecell'><img class='icon' data-type='tooltip' title='"
+								. $row['skillname'] . "\n\n"
+								. $row['skilldesc'] .
+								"' src='"
+								. $row['skillimg'] . 
+								"' width='50' height='50'></td>";
+					}
+
+				?>
+			</tr>
+		</table>
+
+
+	</div>
+
+</body>
