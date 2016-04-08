0cb5f0a6901ba7db11f4d034084ba3198f5b86f9
Jonah-Min <jonahm2472@gmail.com>
2016-04-08 15:10:44 -0400

    Santize inputs
    Remove sql characters as well as html tags.

--- newvalues.php
+++ newvalues.php
@@ -14,6 +14,11 @@
 
 	if (!empty($_POST['name'])) {
 		$name = $_POST['name'];
+		$name = str_replace(')', '', $name);
+		$name = str_replace('\'', '', $name);
+		$name = str_replace(';', '', $name);
+		$name = strip_tags($name);
+
 		$query = "UPDATE champion SET champname='$name' WHERE champname='$champname'";
 		mysql_query($query);
 		$_SESSION['champ'] = $name;
@@ -21,73 +26,133 @@
 
 	if (!empty($_POST['primary'])) {
 		$primary = $_POST['primary'];
+		$primary = str_replace(')', '', $primary);
+		$primary = str_replace('\'', '', $primary);
+		$primary = str_replace(';', '', $primary);
+		$primary = strip_tags($primary);
+
 		$query = "UPDATE champion SET primaryrole='$primary' WHERE champname='$champname'";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['secondary'])) {
 		$primary = $_POST['primary'];
+		$secondary = str_replace(')', '', $secondary);
+		$secondary = str_replace('\'', '', $secondary);
+		$secondary = str_replace(';', '', $secondary);
+		$secondary = strip_tags($secondary);
+
 		$query = "UPDATE champion SET secondary='$primary' WHERE champname='$champname'";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['passivename'])) {
 		$primary = $_POST['passivename'];
+		$primary = str_replace(')', '', $primary);
+		$primary = str_replace('\'', '', $primary);
+		$primary = str_replace(';', '', $primary);
+		$primary = strip_tags($primary);
+
 		$query = "UPDATE skills SET skillname='$primary' WHERE champname='$champname' AND skillnumber=0";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['passivedesc'])) {
 		$primary = $_POST['passivedesc'];
-		$query = "UPDATE skills SET skilldesc='$primary' WHERE champname='$champname' AND skillnumber=0";
+		$primary = str_replace(')', '', $primary);
+		$primary = str_replace('"', '', $primary);
+		$primary = str_replace(';', '', $primary);
+		$primary = strip_tags($primary);
+
+		$query = "UPDATE skills SET skilldesc=\"$primary\" WHERE champname='$champname' AND skillnumber=0";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['Q'])) {
 		$skill = $_POST['Q'];
+		$skill = str_replace(')', '', $skill);
+		$skill = str_replace('\'', '', $skill);
+		$skill = str_replace(';', '', $skill);
+		$skill = strip_tags($skill);
+
 		$query = "UPDATE skills SET skillname='$skill' WHERE champname='$champname' AND skillnumber=1";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['Qdesc'])) {
 		$desc = $_POST['Qdesc'];
-		$query = "UPDATE skills SET skilldesc='$desc' WHERE champname='$champname' AND skillnumber=1";
+		$desc = str_replace(')', '', $desc);
+		$desc = str_replace('"', '', $desc);
+		$desc = str_replace(';', '', $desc);
+		$desc = strip_tags($desc);
+
+		$query = "UPDATE skills SET skilldesc=\"$desc\" WHERE champname='$champname' AND skillnumber=1";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['W'])) {
 		$skill = $_POST['W'];
+		$skill = str_replace(')', '', $skill);
+		$skill = str_replace('\'', '', $skill);
+		$skill = str_replace(';', '', $skill);
+		$skill = strip_tags($skill);
+
 		$query = "UPDATE skills SET skillname='$skill' WHERE champname='$champname' AND skillnumber=2";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['Wdesc'])) {
 		$desc = $_POST['Wdesc'];
-		$query = "UPDATE skills SET skilldesc='$desc' WHERE champname='$champname' AND skillnumber=2";
+		$desc = str_replace(')', '', $desc);
+		$desc = str_replace('"', '', $desc);
+		$desc = str_replace(';', '', $desc);
+		$desc = strip_tags($desc);
+
+		$query = "UPDATE skills SET skilldesc=\"$desc\" WHERE champname='$champname' AND skillnumber=2";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['E'])) {
 		$skill = $_POST['E'];
+		$skill = str_replace(')', '', $skill);
+		$skill = str_replace('\'', '', $skill);
+		$skill = str_replace(';', '', $skill);
+		$skill = strip_tags($skill);
+
 		$query = "UPDATE skills SET skillname='$skill' WHERE champname='$champname' AND skillnumber=3";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['Edesc'])) {
 		$desc = $_POST['Edesc'];
-		$query = "UPDATE skills SET skilldesc='$desc' WHERE champname='$champname' AND skillnumber=3";
+		$desc = str_replace(')', '', $desc);
+		$desc = str_replace('"', '', $desc);
+		$desc = str_replace(';', '', $desc);
+		$desc = strip_tags($desc);
+
+		$query = "UPDATE skills SET skilldesc=\"$desc\" WHERE champname='$champname' AND skillnumber=3";
 		mysql_query($query);
 	}   
 
 	if (!empty($_POST['R'])) {
 		$skill = $_POST['R'];
+		$skill = str_replace(')', '', $skill);
+		$skill = str_replace('\'', '', $skill);
+		$skill = str_replace(';', '', $skill);
+		$skill = strip_tags($skill);
+
 		$query = "UPDATE skills SET skillname='$skill' WHERE champname='$champname' AND skillnumber=4";
 		mysql_query($query);
 	} 
 
 	if (!empty($_POST['Rdesc'])) {
 		$desc = $_POST['Rdesc'];
-		$query = "UPDATE skills SET skilldesc='$desc' WHERE champname='$champname' AND skillnumber=4";
+		$desc = str_replace(')', '', $desc);
+		$desc = str_replace('"', '', $desc);
+		$desc = str_replace(';', '', $desc);
+		$desc = strip_tags($desc);
+
+		$query = "UPDATE skills SET skilldesc=\"$desc\" WHERE champname='$champname' AND skillnumber=4";
 		mysql_query($query);
 	}  
 
